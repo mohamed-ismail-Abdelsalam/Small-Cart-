7fb577d33fa59816a55557fd3ebc6bd4a356f978
‪Mohamed Ismail‬‏ <106432760+mohamed-ismail-Abdelsalam@users.noreply.github.com>
2023-02-28 20:30:26 +0200

    up1

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+var buttons = document.querySelectorAll(".btn_add");
+var cardNumbers = document.querySelector(".items_number");
+var totalPriceOut =document.querySelector(".out_div");
+var totalnumber = 0 ;
+var totalPrice = 0 ;
+
+buttons.forEach(function (button) { 
+    button.addEventListener("click", function (){
+        totalPrice += +(button.getAttribute("price")) ;
+        totalPriceOut.innerHTML = totalPrice;
+        cardNumbers.innerHTML = ++totalnumber;
+        let el = "#element_"+button.getAttribute("number")
+        if (document.querySelector(el).style.display == "") {
+            document.querySelector(el).style.display = "table-row";
+        }
+        document.querySelector(el+" .count").innerHTML = +(document.querySelector(el+" .count").innerHTML)+1;
+        document.querySelector(el+" .total").innerHTML = +(document.querySelector(el+" .count").innerHTML) * +(document.querySelector(el+" .price").innerHTML);
+    });
+})
